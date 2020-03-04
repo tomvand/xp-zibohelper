77f6bb030fc6dfcb981bcbd8b1b0fcf14071145d
tomvand <tomvand@users.noreply.github.com>
2020-03-04 20:13:52 +0100

    (untested) another A/P RST attempt

--- zibohelper.lua
+++ zibohelper.lua
@@ -92,6 +92,8 @@ if PLANE_ICAO == "B738" then
     -- Disconnect A/P and A/T with single button
     logMsg("ZiboHelper: setting up A/P A/T disco command...")
     function ap_at_disco_begin()
+        command_once("laminar/B738/push_button/ap_light_pilot")
+        command_once("laminar/B738/push_button/ap_light_fo")
         command_begin("laminar/B738/autopilot/capt_disco_press")
         command_begin("laminar/B738/autopilot/left_at_dis_press")
     end
