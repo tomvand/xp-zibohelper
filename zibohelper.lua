adae3a6c60dff9e100297af7d895c01ef75971dd
tomvand <tomvand@users.noreply.github.com>
2020-03-04 20:08:44 +0100

    Another failed attempt at A/P RST annunciator cancel

--- zibohelper.lua
+++ zibohelper.lua
@@ -91,27 +91,23 @@ if PLANE_ICAO == "B738" then
 
     -- Disconnect A/P and A/T with single button
     logMsg("ZiboHelper: setting up A/P A/T disco command...")
-    function ap_at_disco()
-        -- Push lights first (so requires second push to silence)
-        command_begin("laminar/B738/push_button/ap_light_pilot")
-        command_once("laminar/B738/push_button/ap_light_pilot")
-        command_end("laminar/B738/push_button/ap_light_pilot")
-        command_begin("laminar/B738/push_button/at_light_pilot")
-        command_once("laminar/B738/push_button/at_light_pilot")
-        command_end("laminar/B738/push_button/at_light_pilot")
-        -- Push disconnect buttons
+    function ap_at_disco_begin()
         command_begin("laminar/B738/autopilot/capt_disco_press")
-        command_once("laminar/B738/autopilot/capt_disco_press")
-        command_end("laminar/B738/autopilot/capt_disco_press")
         command_begin("laminar/B738/autopilot/left_at_dis_press")
+    end
+    function ap_at_disco_continue()
+        command_once("laminar/B738/autopilot/capt_disco_press")
         command_once("laminar/B738/autopilot/left_at_dis_press")
+    end
+    function ap_at_disco_end()
+        command_end("laminar/B738/autopilot/capt_disco_press")
         command_end("laminar/B738/autopilot/left_at_dis_press")
     end
     create_command("zibohelper/ap_at_disco",
                    "Disconnect A/P and A/T, push annunciators",
-                   "ap_at_disco()",
-                   "",
-                   "")
+                   "ap_at_disco_begin()",
+                   "ap_at_disco_continue()",
+                   "ap_at_disco_end()")
     logMsg("ZiboHelper: ok.")
 
 
