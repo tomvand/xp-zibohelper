ad4ca9aa0fd4a9803ad96127d60aede143106a67
tomvand <tomvand@users.noreply.github.com>
2020-04-22 16:48:52 +0200

    Split vis-clouds into vis-clouds and vis-atmo

--- vis-clouds.lua
+++ vis-clouds.lua
@@ -43,18 +43,6 @@ function vis_clouds()
   
       -- TrueHaze (static code only)
       -- https://forums.x-plane.org/index.php?/files/file/41411-fse/
-      --atmosphere parameters
-      set( "sim/private/controls/atmo/atmo_scale_raleigh",        19 )
-      set( "sim/private/controls/atmo/inscatter_gain_mie",     1.0 )
-      set( "sim/private/controls/atmo/inscatter_gain_raleigh",         1.0 )
-      set( "sim/private/controls/atmo/scatter_raleigh_b",         20.0 )
-      set( "sim/private/controls/atmo/scatter_raleigh_g",       10.00 )
-      set( "sim/private/controls/atmo/scatter_raleigh_r",     2.5 )
-      set( "sim/private/controls/hdr/sky_gain",     	3.5 )
-      set( "sim/private/controls/hdr/white_point",     	3.5 )
-      -- set( "sim/private/controls/fog/fog_be_gone",    1.6 )
-      set( "sim/private/controls/terrain/fog_clip_pad",    1500.0 )
-      set( "sim/private/controls/terrain/fog_clip_scale",    0.0 )
       --clouds lighting
       set( "sim/private/controls/clouds/ambient_gain",       1.05 )
       set( "sim/private/controls/clouds/diffuse_gain",       1.39 )
@@ -80,43 +68,6 @@ function vis_clouds()
       -- set( "sim/private/controls/cloud/offscreen_tweak", 1.0)
       -- set( "sim/private/controls/clouds/image_size_x", 4096.0)
       -- set( "sim/private/controls/clouds/image_size_y", 4096.0)
-      --sun parameters
-      set( "sim/private/controls/dome/sun_gain_1_hdr", 1.00)
-      set( "sim/private/controls/dome/sun_gain_2_hdr", 1.90)
-      set( "sim/private/controls/skyc/min_tone_angle", -100)
-      set( "sim/private/controls/skyc/max_tone_angle", -100)
-      --scenery draw distance
-      -- set( "sim/private/controls/skyc/max_dsf_vis_ever", 110000)
-      set( "sim/private/controls/skyc/dsf_fade_ratio", 0.5)
-      set( "sim/private/controls/skyc/dsf_cutover_scale", 3)
-      --SKY COLORS PARAMETERS
-      --clean
-      set( "sim/private/controls/skyc/ambient_ratio_clean", 0.30)
-      set( "sim/private/controls/skyc/direct_ratio_clean", 0.3)
-      set( "sim/private/controls/skyc/mie_scattering_clean", 0.600)
-      set( "sim/private/controls/skyc/raleigh_scattering_clean", 0.3420)
-      set( "sim/private/controls/skyc/shadow_level_clean", 1.00)
-      set( "sim/private/controls/skyc/shadow_offset_clean", 1.00)
-      set( "sim/private/controls/skyc/tone_ratio_clean", 0.8)
-      --mount
-      set( "sim/private/controls/skyc/ambient_ratio_mount", 0.30)
-      set( "sim/private/controls/skyc/direct_ratio_mount", 1.000)
-      set( "sim/private/controls/skyc/mie_scattering_mount", 0.200)
-      set( "sim/private/controls/skyc/raleigh_scattering_mount", 0.29)
-      set( "sim/private/controls/skyc/shadow_level_mount", 1.00)
-      set( "sim/private/controls/skyc/shadow_offset_mount", 1.00)
-      set( "sim/private/controls/skyc/tone_ratio_mount", 0.8)
-      -- tone ratios
-      set( "sim/private/controls/skyc/tone_ratio_foggy", 0.4)
-      set( "sim/private/controls/skyc/tone_ratio_hazy",  0.3)
-      set( "sim/private/controls/skyc/tone_ratio_snowy", 0.5)
-      set( "sim/private/controls/skyc/tone_ratio_ocast", 0.2)
-      set( "sim/private/controls/skyc/tone_ratio_strat", 1.5)
-      set( "sim/private/controls/skyc/tone_ratio_hialt", 0)
-      -- shadow levels
-      -- set( "sim/private/controls/skyc/shadow_level_hazy", 0.6)
-      -- set( "sim/private/controls/skyc/shadow_level_foggy", 0.33)
-      -- set( "sim/private/controls/skyc/shadow_level_ocast", 0.2)
   
   
       -- https://forums.x-plane.org/index.php?/forums/topic/150598-xp11-default-clouds-look-amazing/&tab=comments#comment-1430099
