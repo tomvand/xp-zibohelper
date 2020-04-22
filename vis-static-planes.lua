68a2068104b1b95675e0065b18893c37b0b1fdac
tomvand <tomvand@users.noreply.github.com>
2020-04-22 17:53:37 +0200

    Reduce number of static aircraft for slight performance gain

--- vis-static-planes.lua
+++ vis-static-planes.lua
@@ -3,8 +3,8 @@ local do_once = false
 
 function vis_static_planes()
     if os.clock() > start_time and do_once == false then
-        set("sim/private/controls/park/static_plane_density", 6) -- more intensive
-        set("sim/private/controls/park/static_plane_build_dis", 10000.00)
+        set("sim/private/controls/park/static_plane_density", 1) -- 6 more intensive
+        -- set("sim/private/controls/park/static_plane_build_dis", 10000.00)
 
         do_once = true
     end
