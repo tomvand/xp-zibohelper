11c6d017745ff56f8c42e1d0560c3d8ad49dbe42
Tom van Dijk <tomvand@users.noreply.github.com>
2020-05-02 09:07:32 +0200

    Expand vis-lights with comment on X-Lights

--- vis-lights.lua
+++ vis-lights.lua
@@ -5,6 +5,7 @@ function vis_lights()
     if os.clock() > start_time and do_once == false then
         -- Tuned for TK Ligh Mod 2019
         -- https://forums.x-plane.org/index.php?/files/file/47394-tk-light-mod-2019/
+        -- With X-Lights 1.0.1 **only 1000_lights_close.dds**
         set("sim/private/controls/lights/exponent_near", 0.52)
         set("sim/private/controls/lights/exponent_far", 0.50)
 
