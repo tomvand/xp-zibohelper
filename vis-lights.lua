e1517c7c0faa7e2caaa7b91705a8d23dbcc04850
Tom van Dijk <tomvand@users.noreply.github.com>
2020-05-02 23:11:23 +0200

    First attempt at better light script
    Not completely happy with looks, especially in fog, but clear
    nighttime and transition looks great!

--- vis-lights.lua
+++ vis-lights.lua
@@ -1,16 +1,61 @@
 local start_time = os.clock()
-local do_once = false
 
 function vis_lights()
-    if os.clock() > start_time and do_once == false then
+    if os.clock() > start_time then
         -- Tuned for TK Ligh Mod 2019
         -- https://forums.x-plane.org/index.php?/files/file/47394-tk-light-mod-2019/
         -- With X-Lights 1.0.1 **only 1000_lights_close.dds**
-        set("sim/private/controls/lights/exponent_near", 0.52)
-        set("sim/private/controls/lights/exponent_far", 0.50)
+        --
+        -- This script adapts light brightness (exponent) based on time of day and visibility.
+        
+        -- Constant values
+        set("sim/private/controls/lights/hdr_mix", 0.0) -- Light blob intensity, 0=bright, 1=invisible
+        set("sim/private/controls/lights/dist_near", 200.0) -- Near side of exponent transition
+        set("sim/private/controls/lights/dist_far", 1000.0) -- Far side of exponent transition
 
-        do_once = true
+        -- Inputs
+        local sun_pitch = get("sim/graphics/scenery/sun_pitch_degrees")
+        local visibility = get("sim/graphics/view/visibility_effective_m")
+        local frametime = get("sim/operation/misc/frame_rate_period")
+        local exp_near_now = get("sim/private/controls/lights/exponent_near")
+        local exp_far_now = get("sim/private/controls/lights/exponent_far")
+
+        -- Day/night cycle
+        local night_ratio = -(sun_pitch - 2.2) / (2.2 + 6.5) -- Transition from +2.2deg to -6.5deg
+        if night_ratio < 0.0 then
+            night_ratio = 0.0
+        end
+        if night_ratio > 1.0 then
+            night_ratio = 1.0
+        end
+        local exp_near = (1.0 - night_ratio) * 0.30 + night_ratio * 0.20 -- day: 0.30, night: 0.20
+        local exp_far  = (1.0 - night_ratio) * 0.43 + night_ratio * 0.50 -- day: 0.43, night: 0.50 (very bright)
+
+        -- Fog/visibility
+        local fog_ratio = 1.0 - (visibility - 2000) / (7000 - 2000) -- Transition from 7000m (0.0) to 2000m (1.0)
+        if fog_ratio < 0.0 then
+            fog_ratio = 0.0
+        end
+        if fog_ratio > 1.0 then
+            fog_ratio = 1.0
+        end
+        exp_near = (1.0 - fog_ratio) * exp_near + fog_ratio * 0.50 -- Mix fog exponent 0.50 with current exp
+        exp_far  = (1.0 - fog_ratio) * exp_far  + fog_ratio * 0.30 -- Mix fog exponent 0.30 with current exp
+
+        -- Update datarefs
+        if exp_near_now < exp_near then
+            exp_near_now = exp_near_now + 0.01 * frametime
+        elseif exp_near_now > exp_near then
+            exp_near_now = exp_near_now - 0.01 * frametime
+        end
+        if exp_far_now < exp_far then
+            exp_far_now = exp_far_now + 0.01 * frametime
+        elseif exp_far_now > exp_far then
+            exp_far_now = exp_far_now - 0.01 * frametime
+        end
+        set("sim/private/controls/lights/exponent_near", exp_near_now)
+        set("sim/private/controls/lights/exponent_far", exp_far_now)
     end
 end
 
-do_often("vis_lights()")
+do_every_frame("vis_lights()")
