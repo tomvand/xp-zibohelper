10d46e1df2b2d1bb0c335b7def05d40145d87882
Tom van Dijk <tomvand@users.noreply.github.com>
2020-05-04 19:09:49 +0200

    Retune lights

--- vis-lights.lua
+++ vis-lights.lua
@@ -6,12 +6,12 @@ function vis_lights()
         -- https://forums.x-plane.org/index.php?/files/file/47394-tk-light-mod-2019/
         -- With X-Lights 1.0.1 **only 1000_lights_close.dds**
         --
-        -- This script adapts light brightness (exponent) based on time of day and visibility.
+        -- This script adapts light brightness (exponent) based on sun elevation for more gradual transition.
         
         -- Constant values
         set("sim/private/controls/lights/hdr_mix", 0.0) -- Light blob intensity, 0=bright, 1=invisible
         set("sim/private/controls/lights/dist_near", 200.0) -- Near side of exponent transition
-        set("sim/private/controls/lights/dist_far", 1000.0) -- Far side of exponent transition
+        set("sim/private/controls/lights/dist_far", 10000.0) -- Far side of exponent transition
 
         -- Inputs
         local sun_pitch = get("sim/graphics/scenery/sun_pitch_degrees")
@@ -28,19 +28,19 @@ function vis_lights()
         if night_ratio > 1.0 then
             night_ratio = 1.0
         end
-        local exp_near = (1.0 - night_ratio) * 0.30 + night_ratio * 0.20 -- day: 0.30, night: 0.20
-        local exp_far  = (1.0 - night_ratio) * 0.43 + night_ratio * 0.50 -- day: 0.43, night: 0.50 (very bright)
+        local exp_near = (1.0 - night_ratio) * 0.38 + night_ratio * 0.42 -- day: 0.38 (default), night: 0.42
+        local exp_far  = (1.0 - night_ratio) * 0.42 + night_ratio * 0.50 -- day: 0.42,           night: 0.50 (very bright)
 
         -- Fog/visibility
-        local fog_ratio = 1.0 - (visibility - 2000) / (7000 - 2000) -- Transition from 7000m (0.0) to 2000m (1.0)
-        if fog_ratio < 0.0 then
-            fog_ratio = 0.0
-        end
-        if fog_ratio > 1.0 then
-            fog_ratio = 1.0
-        end
-        exp_near = (1.0 - fog_ratio) * exp_near + fog_ratio * 0.50 -- Mix fog exponent 0.50 with current exp
-        exp_far  = (1.0 - fog_ratio) * exp_far  + fog_ratio * 0.30 -- Mix fog exponent 0.30 with current exp
+        -- local fog_ratio = 1.0 - (visibility - 2000) / (7000 - 2000) -- Transition from 7000m (0.0) to 2000m (1.0)
+        -- if fog_ratio < 0.0 then
+        --     fog_ratio = 0.0
+        -- end
+        -- if fog_ratio > 1.0 then
+        --     fog_ratio = 1.0
+        -- end
+        -- exp_near = (1.0 - fog_ratio) * exp_near + fog_ratio * 0.50 -- Mix fog exponent 0.50 with current exp
+        -- exp_far  = (1.0 - fog_ratio) * exp_far  + fog_ratio * 0.30 -- Mix fog exponent 0.30 with current exp
 
         -- Update datarefs
         if exp_near_now < exp_near then
