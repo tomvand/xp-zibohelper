24a361ee2bd3e403793459da6b936347763b649a
Tom van Dijk <tomvand@users.noreply.github.com>
2020-06-07 15:42:55 +0200

    "Fix" A/P A/T disco
    Not ideal as A/T is now silenced immediately, but at least the
    A/P annunciator can be silenced.

--- zibohelper-ap-at-disco.lua
+++ zibohelper-ap-at-disco.lua
@@ -1,27 +1,16 @@
 if PLANE_ICAO == "B738" then
-    dataref("dr_cmd_a", "laminar/B738/autopilot/cmd_a_status")
-    dataref("dr_cmd_b", "laminar/B738/autopilot/cmd_b_status")
+    dataref("dr_ap_disco_lt", "laminar/B738/annunciator/ap_disconnect1")
+    dataref("dr_at_disco_lt", "laminar/B738/annunciator/at_disconnect1")
     dataref("dr_at", "laminar/B738/autopilot/autothrottle_status")
 
-    -- Disconnect A/P and A/T with single button
-    logMsg("ZiboHelper: setting up A/P A/T disco command...")
-    function ap_at_disco()
-        if dr_cmd_a > 0 or dr_cmd_b > 0 or dr_at > 0 then
-            if dr_cmd_a > 0 or dr_cmd_b > 0 then
-                command_once("laminar/B738/autopilot/capt_disco_press")
-            end
-            if dr_at > 0 then
-                command_once("laminar/B738/autopilot/left_at_dis_press")
-            end
-        else
-            command_once("laminar/B738/push_button/ap_light_pilot")
-            command_once("laminar/B738/push_button/at_light_pilot")
+    -- Disconnect A/T if A/P is disengaged
+    function ap_at_disco_loop()
+        if dr_ap_disco_lt > 0 and dr_at > 0 then
+            command_once("laminar/B738/autopilot/left_at_dis_press")
+        end
+        if dr_at_disco_lt > 0 and dr_ap_disco_lt == 0 then
+            command_once("laminar/B738/autopilot/left_at_dis_press") -- silence warning
         end
     end
-    create_command("zibohelper/ap_at_disco",
-                   "Disconnect A/P and A/T, push annunciators",
-                   "ap_at_disco()",
-                   "",
-                   "")
-    logMsg("ZiboHelper: ok.")
+    do_every_frame("ap_at_disco_loop()")
 end
--- zibohelper-ap-at-engage.lua
+++ zibohelper-ap-at-engage.lua
@@ -1,13 +1,13 @@
 if PLANE_ICAO == "B738" then
-    dataref("dr_cmd_a", "laminar/B738/buttons/autopilot/cmd_a")
-    dataref("dr_cmd_b", "laminar/B738/buttons/autopilot/cmd_b")
-    dataref("dr_at_arm_lt", "laminar/B738/autopilot/autothrottle_status")
+    dataref("dr_cmd_a", "laminar/B738/autopilot/cmd_a_status")
+    dataref("dr_cmd_b", "laminar/B738/autopilot/cmd_b_status")
+    dataref("dr_at", "laminar/B738/autopilot/autothrottle_status")
 
     -- Engage CMD A, engage A/T on second press
     function ap_at_engage()
         if dr_cmd_a == 0 and dr_cmd_b == 0 then
             command_once("laminar/B738/autopilot/cmd_a_press")
-        else
+        elseif dr_at == 0 then
             command_once("laminar/B738/autopilot/autothrottle_arm_toggle")
         end
     end
