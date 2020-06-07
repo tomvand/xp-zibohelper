d5d5887a518c17e24e03997e20bf33878f5f2e89
Tom van Dijk <tomvand@users.noreply.github.com>
2020-06-07 14:39:55 +0200

    FIX taxi-in flow
    Mistake in separating scripts, plus zibo datarefs changed.

--- zibohelper-toggle-both-fds.lua
+++ zibohelper-toggle-both-fds.lua
@@ -1,16 +1,20 @@
 if PLANE_ICAO == "B738" then
-    dataref("dr_fd_cpt", "laminar/B738/switches/autopilot/fd_ca", "writeable")
-    dataref("dr_fd_fo", "laminar/B738/switches/autopilot/fd_fo", "writeable")
+    dataref("dr_fd_cpt", "laminar/B738/autopilot/pfd_fd_cmd")
+    dataref("dr_fd_fo", "laminar/B738/autopilot/pfd_fd_cmd_fo")
 
     -- Toggle both FDs
     logMsg("ZiboHelper: setting up FD toggle command...")
     function fd_toggle()
         if dr_fd_cpt == 0 then
-            dr_fd_cpt = 1
-            dr_fd_fo = 1
+            command_once("laminar/B738/autopilot/flight_director_toggle")
+            if dr_fd_fo == 0 then
+                command_once("laminar/B738/autopilot/flight_director_fo_toggle")
+            end
         else
-            dr_fd_fo = 0
-            dr_fd_cpt = 0
+            if dr_fd_fo == 1 then
+                command_once("laminar/B738/autopilot/flight_director_fo_toggle")
+            end
+            command_once("laminar/B738/autopilot/flight_director_toggle")
         end
     end
     create_command("zibohelper/fd_toggle_both_cpt",
