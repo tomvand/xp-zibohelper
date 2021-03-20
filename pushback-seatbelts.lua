51ffd0574d4834404caff8cd3ce446f52f1b77c4
Tom van Dijk <tomvand@users.noreply.github.com>
2021-03-20 19:52:56 +0100

    Script to toggle seatbelts and pushback

---
+++ pushback-seatbelts.lua
@@ -0,0 +1,35 @@
+local seatbelts_cmd = "sim/systems/seatbelt_sign_toggle"
+
+if PLANE_ICAO == "A320" then
+  seatbelts_cmd = "a320helper/seatbelts_toggle"
+end
+
+local PUSHBACK_STATE_CONNECT = 0
+local PUSHBACK_STATE_START = 1
+local PUSHBACK_STATE_DONE = 2
+local pushback_state = PUSHBACK_STATE_CONNECT
+
+local function is_seatbelts_on()
+  if get("sim/cockpit2/switches/fasten_seat_belts") > 0.5 then
+    return true
+  elseif get("a320/Overhead/LightBelts") > 0.5 then
+    return true
+  end
+  return false
+end
+
+function pushback_seatbelts_begin()
+  if pushback_state < PUSHBACK_STATE_DONE and not is_seatbelts_on() then
+    command_once(seatbelts_cmd)
+  elseif pushback_state == PUSHBACK_STATE_CONNECT then
+    command_once("BetterPushback/connect_first")
+    pushback_state = PUSHBACK_STATE_START
+  elseif pushback_state == PUSHBACK_STATE_START then
+    command_once("BetterPushback/start")
+    pushback_state = PUSHBACK_STATE_DONE
+  else
+    command_once(seatbelts_cmd)
+  end
+end
+create_command("misc/pushback_seatbelts_toggle", "Start pushback, toggle seatbelts",
+    "pushback_seatbelts_begin()", "", "")
