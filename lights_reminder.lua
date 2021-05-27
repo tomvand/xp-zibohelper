7cfaf61880cf1dad5d86897abbd656134e4fc334
Tom van Dijk <tomvand@users.noreply.github.com>
2021-05-27 18:11:07 +0200

    [lights_reminder.lua] make more flexible using anonymous functions

--- lights_reminder.lua
+++ lights_reminder.lua
@@ -1,17 +1,18 @@
 require("graphics")
 
-local xplm_landing_lights = nil
-local xplm_taxi_lights = nil
-local xplm_strobe = nil
-local xplm_beacon = nil
-local xplm_parkbrake = nil
-local xplm_onground = nil
+local get_landing_lights = nil
+local get_taxi_lights = nil
+local get_strobe = nil
+local get_beacon = nil
+local get_parkbrake = nil
+local get_onground = nil
 
 local current_landing_lights = false
 local current_taxi_lights = false
 local current_strobe = false
 local current_beacon = false
 local current_parkbrake = false
+local current_onground = true
 
 local datarefs_loaded = false
 
@@ -27,47 +28,49 @@ local function lr_load_datarefs()
     return true
   end
 
-  if xplm_onground == nil then
-    xplm_onground = XPLMFindDataRef("sim/flightmodel/failures/onground_any")
+  if get_onground == nil then
+    local xplm_onground = XPLMFindDataRef("sim/flightmodel/failures/onground_any")
+    if xplm_onground ~= nil then
+      get_onground = function() return XPLMGetDatai(xplm_onground) == 1; end
+    end
     return false
   end
-
+  
   if PLANE_ICAO == "A320" then
-    if xplm_landing_lights == nil then
-      xplm_landing_lights = XPLMFindDataRef("a320/Overhead/LightLandL")
+    if get_landing_lights == nil then
+      local xplm_landing_lights = XPLMFindDataRef("a320/Overhead/LightLandL")
+      if xplm_landing_lights ~= nil then
+        get_landing_lights = function() return XPLMGetDataf(xplm_landing_lights) > 1.5; end
+      end
       return false
-    elseif xplm_taxi_lights == nil then
+    elseif get_taxi_lights == nil then
       xplm_taxi_lights = XPLMFindDataRef("a320/Overhead/LightNose")
+      if xplm_taxi_lights ~= nil then
+        get_taxi_lights = function() return XPLMGetDataf(xplm_taxi_lights) > 0.5; end
+      end
       taxi_lights_label = "N"
       return false
-    elseif xplm_strobe == nil then
-      xplm_strobe = XPLMFindDataRef("a320/Overhead/LightStrobe")
+    elseif get_strobe == nil then
+      xplm_strobe = XPLMFindDataRef("sim/cockpit/electrical/strobe_lights_on")
+      if xplm_strobe ~= nil then
+        get_strobe = function() return XPLMGetDatai(xplm_strobe) == 1; end
+      end
       return false
-    elseif xplm_beacon == nil then
+    elseif get_beacon == nil then
       xplm_beacon = XPLMFindDataRef("a320/Overhead/LightBeacon")
+      if xplm_beacon ~= nil then
+        get_beacon = function() return XPLMGetDataf(xplm_beacon) > 0.5; end
+      end
       return false
-    elseif xplm_parkbrake == nil then
+    elseif get_parkbrake == nil then
       xplm_parkbrake = XPLMFindDataRef("a320/Pedestal/ParkBrake")
+      if xplm_parkbrake ~= nil then
+        get_parkbrake = function() return XPLMGetDataf(xplm_parkbrake) > 0.5; end
+      end
       return false
     end
   elseif PLANE_ICAO == "B738" then
-    if xplm_landing_lights == nil then
-      xplm_landing_lights = XPLMFindDataRef("laminar/B738/switch/land_lights_left_pos")
-      return false
-    elseif xplm_taxi_lights == nil then
-      xplm_taxi_lights = XPLMFindDataRef("a320/Overhead/LightNose")
-      taxi_lights_label = "N"
-      return false
-    elseif xplm_strobe == nil then
-      xplm_strobe = XPLMFindDataRef("a320/Overhead/LightStrobe")
-      return false
-    elseif xplm_beacon == nil then
-      xplm_beacon =  XPLMFindDataRef("a320/Overhead/LightBeacon")
-      return false
-    elseif xplm_parkbrake == nil then
-      xplm_parkbrake = XPLMFindDataRef("a320/Pedestal/ParkBrake")
-      return false
-    end
+    -- TODO
   else
     -- TODO
   end
@@ -81,13 +84,13 @@ function lights_reminder_on_frame()
     return
   end
 
-  local landing_lights = XPLMGetDataf(xplm_landing_lights) > 0.5
-  local taxi_lights = XPLMGetDataf(xplm_taxi_lights) > 0.5
-  local strobe = XPLMGetDataf(xplm_strobe) > 0.5
-  local beacon = XPLMGetDataf(xplm_beacon) > 0.5
+  local landing_lights = get_landing_lights()
+  local taxi_lights = get_taxi_lights()
+  local strobe = get_strobe()
+  local beacon = get_beacon()
 
-  onground = XPLMGetDatai(xplm_onground) == 1
-  current_parkbrake = XPLMGetDataf(xplm_parkbrake) > 0.5
+  current_parkbrake = get_parkbrake()
+  current_onground = get_onground()  
 
   -- Detect changes
   if landing_lights ~= current_landing_lights then
@@ -108,13 +111,16 @@ function lights_reminder_on_frame()
   end
 
   -- Always show in these circumstances
-  if onground and (landing_lights or strobe) then
+  if current_onground and (landing_lights or strobe) then
+    reminder_timeout = os.clock() + TIMEOUT
+  end
+  if not current_onground and not (strobe and beacon) then
     reminder_timeout = os.clock() + TIMEOUT
   end
   if landing_lights and not taxi_lights then
     reminder_timeout = os.clock() + TIMEOUT
   end
-  if parkbrake and not beacon then
+  if not current_parkbrake and not beacon then
     reminder_timeout = os.clock() + TIMEOUT
   end
 end
@@ -122,7 +128,7 @@ do_every_frame("lights_reminder_on_frame()")
 
 
 function lights_reminder_on_draw()
-  if os.clock() > reminder_timeout + 1.0 then
+  if os.clock() > reminder_timeout + FADEOUT_TIME then
     return
   end
 
@@ -163,7 +169,7 @@ function lights_reminder_on_draw()
   if current_beacon then
     glColor4f(0.0, 1.0, 0.0, alpha)
   elseif not current_parkbrake then
-    glColor4f(1.0, 1.0, 1.0, alpha)
+    glColor4f(1.0, 0.0, 0.0, alpha)
   else
     glColor4f(1.0, 1.0, 1.0, alpha)
   end
